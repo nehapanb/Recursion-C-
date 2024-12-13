f4c932c95f9a70dd62a8dcfa334f3141ac9d556a
Neha panbude <nehapanbude123@gmail.com>
2024-12-13 13:03:52 +0530

    first commit

---
+++ term.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int fib (int a)
+{
+
+    if (a==0)
+    {
+
+        return 1;
+    }
+    if(a<0)
+    {
+        return 0;
+    }
+    return fib (a-1) + fib(a-2);
+
+}
+
+int main()
+{
+    int a;
+    cout<<"enter any no for fibonacci\n";
+
+    cin>>a;
+
+  cout<<fib(a);
+}
