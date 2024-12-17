1bde5bd7d7c0ca7c040ba3eee081930f70505e8c
Neha panbude <nehapanbude123@gmail.com>
2024-12-17 13:15:22 +0530

    first commit

---
+++ overloading.cpp
@@ -0,0 +1,9 @@
+
+
+//satic data member and static member function is a attribute of a class.while non static data member and non static member function is 
+//a attribute of an object.
+//Whenever we call static member we need not create and object.we can access with the name of class using scope resolution opreater.
+
+//types of classes.
+
+//Single Class if a programme having only class is called single class.
