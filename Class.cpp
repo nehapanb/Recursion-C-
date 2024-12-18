5f13d5d6ed36785a463058866a3c0632e34e630e
Neha panbude <nehapanbude123@gmail.com>
2024-12-18 12:59:54 +0530

    first commit

--- Class.cpp
+++ Class.cpp
@@ -14,3 +14,17 @@
 // Object 
 
 // Instance  of a class is called an object .object is real world entity.
+
+
+//satic data member and static member function is a attribute of a class.while non static data member and non static member function is 
+//a attribute of an object.
+//Whenever we call static member we need not create and object.we can access with the name of class using scope resolution opreater.
+
+//types of classes.
+
+//Single Class if a programme having only class is called single class.
+// if a programme is having more than one class it is called as multi class.
+// Anonymous class name less class is called anonymous class.
+//composit class having object of privious class.And we call the function of privious class inside the member function of composit class.
+
+
